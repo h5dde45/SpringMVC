a5cabd0ba995e00fb96b0fe22e773135a39813c3
nov <nov@mail.ru>
2017-04-29 22:27:55 +0400

    + admin

--- src/main/java/ru/javabegin/training/objects/User.java
+++ src/main/java/ru/javabegin/training/objects/User.java
@@ -1,23 +1,32 @@
 package ru.javabegin.training.objects;
 
 public class User {
-	private String name;
+    private String name;
 
-	private String password;
+    private String password;
+    private boolean admin;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
 }
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -8,6 +8,7 @@
 
 <p>Hello ${user.name}!</p>
 <p>Your password is  ${user.password}!</p>
+<p>Admin  ${user.admin}!</p>
 
 
 
