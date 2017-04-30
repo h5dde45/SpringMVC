c4b729156bbc55d77e31f6f98ba6a4699fa78da9
nov <nov@mail.ru>
2017-04-30 17:35:18 +0400

    ...

--- src/main/java/ru/javabegin/training/controllers/LoginController.java
+++ src/main/java/ru/javabegin/training/controllers/LoginController.java
@@ -1,42 +1,59 @@
 package ru.javabegin.training.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.javabegin.training.objects.User;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
 public class LoginController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView main(HttpSession session) {
-
-        return new ModelAndView("login", "user",
-                new User());
+    public ModelAndView main() {
+        return new ModelAndView("login", "user", new User());
     }
 
     @RequestMapping(value = "/check-user", method = RequestMethod.POST)
-    public String checkUser(@Valid @ModelAttribute("user") User user,
-                                  BindingResult bindingResult,
-                                  Model model) {
+    public String checkUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "login";
         }
-        model.addAttribute("user",user);
-        return "main";    }
+
+        model.addAttribute("user", user);
+
+        return "main";
+    }
 
     @RequestMapping(value = "/failed", method = RequestMethod.GET)
     public ModelAndView failed() {
-        return new ModelAndView("login-failed",
-                "message", "Login failed");
+        return new ModelAndView("login-failed", "message", "Login failed!");
+    }
+
+
+    @RequestMapping(value = "/get-json-user/{name}/{admin}", method = RequestMethod.GET,
+            produces = "application/json")
+    @ResponseBody
+    public User getJsonUser(@PathVariable("name") String name) {
+        User user = new User();
+        user.setName(name);
+        return user;
+    }
+
+    @RequestMapping(value = "/put-json-user",
+            method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity<String> setJsonUser(@RequestBody User user) {
+        System.out.println(user.getName());
+        return new ResponseEntity<String>(HttpStatus.OK);
     }
 
 }
